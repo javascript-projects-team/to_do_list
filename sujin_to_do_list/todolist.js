7de698ea8d65d1c8274151ec39284d311265f4ff
sujin <sujin@sujinui-MacBookPro.local>
2024-03-31 15:09:55 +0900

    create element add event

--- sujin_to_do_list/todolist.js
+++ sujin_to_do_list/todolist.js
@@ -1,23 +1,20 @@
-let a = 1;
+const delButtonClickEvent = () => {
+  alert(`delete button event`);
+};
 
-function completeList() {
-  console.log(' check your box');
-}
-
-function addList() {
-  console.log(' click your button');
-  a++;
-
-  let chk = document.createElement('input');
-  chk.setAttribute('type', 'checkbox');
-  chk.addEventListener('onclick', completeList());
-
-  let input = document.createElement('input');
-  input.setAttribute('placeholder', '일정을 입력하세요');
-
-  let liTag = document.createElement('li');
-  liTag.setAttribute('class', 'main_li_list');
-  liTag.appendChild(chk);
-  liTag.appendChild(input);
-  document.getElementById('main_ul').appendChild(liTag);
-}
+const addButtonClickEvent = () => {
+  const list = document.getElementById(`to_do_list`);
+  const radioInput = document.createElement(`input`);
+  radioInput.setAttribute(`type`, `radio`);
+  list.append(radioInput);
+  const textInput = document.createElement(`input`);
+  textInput.setAttribute(`type`, ``);
+  textInput.setAttribute(`class`, `text_area`);
+  list.append(textInput);
+  const delBtn = document.createElement(`button`);
+  delBtn.append(`삭제`);
+  delBtn.setAttribute(`class`, `del_btn`);
+  delBtn.setAttribute(`onclick`, `delButtonClickEvent()`);
+  list.append(delBtn);
+  list.append(document.createElement(`br`));
+};
